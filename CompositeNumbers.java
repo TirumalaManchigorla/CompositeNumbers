5b7bdaed7fdbd9f94f908d6277e4eddc3d724335
TirumalaManchigorla <139000551+TirumalaManchigorla@users.noreply.github.com>
2023-12-15 16:47:29 +0530

    Update CompositeNumbers.java

--- CompositeNumbers.java
+++ CompositeNumbers.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class CompositeNumbers
-	{
+{
     public static void main(String[] args)
 	{
         Scanner sc = new Scanner(System.in);
@@ -9,27 +9,22 @@ class CompositeNumbers
         int min = sc.nextInt();
         System.out.println("Enter maximum value:");
         int max = sc.nextInt();
-      //  int compositeCount = 0;
-
         System.out.println("Composite numbers between " + min + " and " + max + ":");
 
         for (int num = min; num <= max; num++) 
-		{
+	{
             int count = 0;
             for (int i = 1; i <= num; i++)
-			{
+	    {
                 if (num % i == 0) 
-				{
+		{
                     count++;
                 }
             }
            if (count > 2)
-			{ // A number is composite if it has more than two divisors
+	    { // A number is composite if it has more than two divisors
                 System.out.print(num + " ");
-               // compositeCount++;
             }
         }
-
-       // System.out.println("\nNumber of composite numbers: " + compositeCount);
     }
 }
